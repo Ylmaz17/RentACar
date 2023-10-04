77a14dcd80b5d7c7e16943b0cf79206ba3378641
İsmail <iylmz1707@gmail.com>
2023-10-04 19:41:09 +0300

    Entity Configuration and Repository Implementation

---
+++ Persistence/Repositories/CarRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Services.Repositories;
+
+namespace Persistence.Repositories;
+
+public class CarRepository : EfRepositoryBase<Car, Guid, BaseDbContext>, ICarRepository
+{
+    public CarRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
---
+++ Persistence/Repositories/ModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Persistence.Repositories;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Services.Repositories;
+using Domain.Entities;
+
+namespace Persistence.Repositories;
+
+public class ModelRepository : EfRepositoryBase<Model, Guid, BaseDbContext>, IModelRepository
+{
+    public ModelRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
